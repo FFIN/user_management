22b454c791d856614622828459d8cbb409a7f0b4
finn <inoue@fieldweb.co.jp>
2015-04-24 16:39:10 +0900

    Update change log

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer       'Yosuke INOUE'
 maintainer_email 'inoue@fieldweb.co.jp'
 license          'Apache 2.0'
 description      'Installs/Configures user accounts'
-version          '1.0.0'
+version          '1.0.1'
 
 %w{ centos redhat fedora }.each do |os|
   supports os
